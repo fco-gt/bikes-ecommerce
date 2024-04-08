58251a4c5b0d223905de87568ab72e21b686de28
Dxstiz <143524146+Dxstiz@users.noreply.github.com>
2024-04-08 17:11:53 -0400

    Update slider.ts

--- config/slider.ts
+++ config/slider.ts
@@ -2,8 +2,8 @@ export const slider = [
   {
     name: "Bicicleta 27” Mountain Bike",
     backText: "Mountain",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    more: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description: "Desafiando Terrenos: Aventura sin Límites",
+    more: "La Bicicleta Mountain Bike 27” te lleva más allá de los límites. Diseñada para desafiar terrenos abruptos con precisión y estilo. Confort, resistencia y rendimiento se fusionan en este compañero de aventuras. ¡Explora sin límites!",
     price: 99.99,
     img: "mountain-bike-1.png",
     color: "26BCC6",
@@ -11,8 +11,8 @@ export const slider = [
   {
     name: "Bicicleta Ciclismo 9 Velocidades",
     backText: "Ciclismo",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    more: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description: "Rutas Desafiantes: Ciclismo de 9 Velocidades",
+    more: "La Bicicleta de Ciclismo de 9 Velocidades te impulsa hacia nuevas alturas. Diseñada para desafiar terrenos diversos con precisión y estilo. Confort, resistencia y rendimiento se combinan en este compañero de ruta. ¡Explora cada camino con confianza!",
     price: 99.99,
     img: "city-bike-1.png",
     color: 'AB5C00',
@@ -20,8 +20,8 @@ export const slider = [
   {
     name: "Bicicleta BMX",
     backText: "BMX",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    more: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    description: "Acrobacias Urbanas: Elegancia y Agilidad",
+    more: "La BMX, ágil y robusta, desafia la gravedad con su chasis de acero. Ruedas resistentes y frenos precisos ofrecen control absoluto. Su diseño, tanto elegante como funcional, la convierte en la compañera ideal para las acrobacias urbanas más audaces.",
     price: 99.99,
     img: "bmx-1.png",
     color: '00B17C'
