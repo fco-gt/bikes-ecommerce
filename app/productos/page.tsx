ed0f2be4b954c589cd78c20d5a9f491cd8d4a2c3
Francisco <74993776+fco-gt@users.noreply.github.com>
2024-04-09 21:55:37 -0400

    Add Catalog Button Page Up

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "@nextui-org/input";
 import {
   Button,
@@ -18,8 +18,34 @@ import ProductList from "@/components/ProductList";
 
 import { CiSearch } from "react-icons/ci";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { FaArrowUp } from "react-icons/fa";
 
 export default function Page() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled down
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  // Scroll to top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="mt-0 md:mt-9">
       <section className="flex items-center justify-between">
@@ -95,6 +121,23 @@ export default function Page() {
         <Sidebar />
         <ProductList />
       </div>
+
+      {isVisible ? (
+        <button
+          onClick={scrollToTop}
+          type="button"
+          className="fixed z-[9999999] bottom-5 right-5 p-2 bg-[#26bcc6] border-zinc-700 border-2 text-[#252525] rounded-full focus:outline-none transition duration-500 ease-in-out transform opacity-100 scale-100 px-9"
+        >
+          <FaArrowUp size={15} />
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="fixed z-[9999999] bottom-5 right-5 p-2 bg-[#26bcc6] text-white rounded-full focus:outline-none transition duration-500 ease-in-out transform opacity-0 scale-50"
+        >
+          <FaArrowUp size={15} />
+        </button>
+      )}
     </div>
   );
 }
